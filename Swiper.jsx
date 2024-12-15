b0617cf1f4340285735ced64c34716eb067ba776
Kabilan P <104247633+Kabilanp4@users.noreply.github.com>
2024-12-15 18:33:39 +0530

    Swiper-https://swiperjs.com/

---
+++ Swiper.jsx
@@ -0,0 +1,42 @@
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Navigation, Pagination } from "swiper/modules";
+
+<Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {filteredCategory?.length > 0 &&
+          filteredCategory.map((book, index) => (
+            <SwiperSlide key={index}>
+              <Bookcard key={index} book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
